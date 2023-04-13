ac1e576c1004511b3583315eb8064c61ec05f069
furnn <gka06566@gmail.com>
2023-04-13 19:32:55 +0900

    plotly_graph_done

--- monthly_proj/eda/views.py
+++ monthly_proj/eda/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly
+from io import StringIO
 
 def index_view(request):
-    return render(request, 'index.html', {fig})
+    context={}
+
+    x = np.linspace(0, 10, 100)
+    y = x * 2
+    fig = go.Figure(data=go.Line(x=x, y=y))
+
+    graph_div = plotly.offline.plot(fig, auto_open = False, output_type="div")
+    context['grp3']=graph_div
+    return render(request, 'index.html', context)
