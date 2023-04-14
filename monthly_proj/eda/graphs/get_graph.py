15cee1a4a2a177574a474e8b6c25eac13cdf1f74
JJooKim <rladudwn0625@gmail.com>
2023-04-14 18:48:07 +0900

    최종 all merged togther

--- monthly_proj/eda/graphs/get_graph.py
+++ monthly_proj/eda/graphs/get_graph.py
@@ -1,99 +1,214 @@
 from typing import Tuple, Dict
+from plotly.graph_objs._figure import Figure
 import os
 
 import numpy as np
 import pandas as pd
-import chart_studio
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.graph_objs._figure import Figure
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from setting import CHART_STUDIO_USERNAME, CHART_STUDIO_API
-
-chart_studio.tools.set_credentials_file(username=CHART_STUDIO_USERNAME, api_key=CHART_STUDIO_API)
 
 path = os.path.dirname(os.path.realpath(__file__))
 cat_data = pd.read_csv(os.path.join(path, 'data', 'categorical_data.csv'))
 num_data = pd.read_csv(os.path.join(path, 'data', 'numeric_data.csv'))
 
 
-def get_pie_foundry_dittr() -> Figure:
-    values = cat_data['Foundry'].value_counts()
-    names = values.index
-    fig = px.pie(names=names, values=values, hole=0.2)
-    fig.update_layout({
-        'title': {
-            'text': 'Foundry Distribution',
-            'x': 0.5
-        }
-    })
+def fig_layout(fig, l='Legend',                 # figure 객체, 제목, 제목 크기 
+               f_s=15, f_c='black',             # 글자 크기, 글자 색깔
+               paper_c='', plot_c=''):          # 배경 색깔, 그래프 바탕 색깔
+    fig.update_layout(
+            legend = {
+                'title' : l
+            },
+            font = {
+                'size' : f_s,
+                'color' : f_c
+            },
+            # paper_bgcolor = paper_c,
+            # plot_bgcolor = plot_c
+        )
 
+
+def get_pie(names, values, hole, legend="", **kwargs):
+    fig = px.pie(names=names, values=values, hole=hole, **kwargs)
+    fig_layout(fig, l=legend)
     return fig
 
 
-def get_bar_foundry_dittr() -> Figure:
-    values = cat_data['Foundry'].value_counts()
-    names = values.index
-    fig = px.bar(y=names, x=values, color=names)
-    fig.update_layout({
-        'title': {
-            'text': 'foundry_category',
-            'x': 0.5
-        }
-    })
-
+def get_bar(x, y, color, legend="", **kwargs):
+    fig = px.bar(x=x, y=y, color=color, **kwargs)
+    fig_layout(fig, l=legend)
     return fig
 
 
-def get_corr() -> Figure:
-    corr = num_data.corr().round(2)
-    fig = px.imshow(corr, text_auto=True)
+def get_scatter(data, x, y, color=None, legend="", **kwargs):
+    fig = px.scatter(data_frame=data, x=x, y=y, color=color, **kwargs)
+    fig_layout(fig, l=legend)
+    return fig    
 
+
+def get_hist(data, x, legend="", **kwargs):
+    fig = px.histogram(data_frame=data, x=x, **kwargs)
+    fig_layout(fig, l=legend)
     return fig
 
 
-def get_scatter(data:pd.DataFrame, x:str, y:str, **kwargs) -> Figure:
-    fig = px.scatter(data_frame=data, y=y, x=x, **kwargs)
+def get_box(data, x, color, legend="", **kwargs):
+    fig = px.box(data_frame=data, x=x, color=color, **kwargs)
+    fig_layout(fig, l=legend)
     return fig
 
-def return_figures1():
-    fig1 = get_pie_foundry_dittr()
-    fig2 = get_bar_foundry_dittr()
-    fig3 = get_corr()
-    fig4 = get_scatter(cat_data, x="Die Size (mm^2)", y="TDP (W)", trendline="ols")
-    fig5 = get_scatter(cat_data, x="Die Size (mm^2)", y="Transistors (million)", trendline="ols")
-    fig6 = get_scatter(cat_data, x="Release Date", y="Freq (MHz)", color="Type")
-    return fig1, fig2, fig3, fig4, fig5, fig6
+
+# 프로세서 타입에 따른 그래프
+def get_graph_by_type():
+    graphs = []
+    # title Type Distribution
+    graphs.append(get_pie(names=cat_data['Type'].value_counts().index,
+                          values=cat_data['Type'].value_counts(),
+                          hole=0.3,
+                          legend='Type'))
+    # title 2 in 1
+    graphs.append(get_box(data=cat_data, 
+                          x='Process Size (nm)', 
+                          color='Vendor', 
+                          legend='Vendor', 
+                          facet_col='Type'))
+    # title Processor Size changes Over Time
+    graphs.append(get_scatter(data=cat_data,
+                              x='Release Date',
+                              y='Process Size (nm)',
+                              color='Type',
+                              legend='Type'))
+    # title TDP Change Over Time By Processor Type
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='TDP (W)', 
+                              color='Type', 
+                              legend='Type'))
+    # title Die Size Change Over Time By Processor Type
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='Die Size (mm^2)', 
+                              color='Type', 
+                              legend='Type'))
+    # title Transistor Change Over Time By Processor Type
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='Transistors (million)', 
+                              color='Type', 
+                              legend='Type'))
+    # title Frequency Change Over Time By Processor Type
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='Freq (MHz)', 
+                              color='Type', 
+                              legend='Type'))
+    # title Processor Data Correlation Coefficient
+    graphs.append(px.imshow(num_data.corr().round(2), text_auto=True))
+    return graphs
+
+
+# 공급 업체에 따른 그래프
+def get_graph_by_vendor():
+    graphs = []
+    # title Size Distribution By Vendor
+    graphs.append(get_box(data=cat_data, 
+                          x='Process Size (nm)', 
+                          color='Vendor', 
+                          legend='Vendor'))
+    # title Processor Size Change By Vendor    
+    graphs.append(get_scatter(data=cat_data,
+                              x='Release Date',
+                              y='Process Size (nm)',
+                              color='Vendor',
+                              legend='Vendor'))
+    # title TDP Change Over Time By Vendor
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='TDP (W)', 
+                              color='Vendor', 
+                              legend='Vendor'))
+    # title Die Size Change Over Time By Vendor
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='Die Size (mm^2)', 
+                              color='Vendor', 
+                              legend='Vendor'))
+    # title Transistor Change Over Time By Vendor
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='Transistors (million)', 
+                              color='Vendor', 
+                              legend='Vendor'))
+    # title Frequency Change Over Time By Vendor
+    graphs.append(get_scatter(data=cat_data,
+                              x='Release Date',
+                              y='Freq (MHz)',
+                              legend='Vendor',
+                              color='Vendor'))    
+    return graphs
+
+
+# 주조 업체에 따른 그래프
+def get_graph_by_foundry():
+    graphs = []
+    # title Foundry Distribution
+    graphs.append(get_pie(names=cat_data['Foundry'].value_counts().index,
+                          values=cat_data['Foundry'].value_counts(),
+                          hole=0.3,
+                          legend='Foundry'))
+    # title Processor Size Distribution By Foundry
+    graphs.append(get_box(data=cat_data, 
+                          x='Process Size (nm)', 
+                          color='Foundry', 
+                          legend='Foundry'))
+    # title Processor Size Change By Foundry    
+    graphs.append(get_scatter(data=cat_data,
+                              x='Release Date',
+                              y='Process Size (nm)',
+                              color='Foundry',
+                              legend='Foundry'))
+    # title TDP Change Over Time By Foundry
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='TDP (W)', 
+                              color='Foundry', 
+                              legend='Foundry'))
+    # title Die Size Change Over Time By Foundry
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='Die Size (mm^2)', 
+                              color='Foundry', 
+                              legend='Foundry'))
+    # title Transistor Change Over Time By Foundry
+    graphs.append(get_scatter(data=cat_data, 
+                              x='Release Date', 
+                              y='Transistors (million)', 
+                              color='Foundry', 
+                              legend='Foundry'))
+    # title Frequency Change Over Time By Foundry
+    graphs.append(get_scatter(data=cat_data,
+                              x='Release Date',
+                              y='Freq (MHz)',
+                              legend='Foundry',
+                              color='Foundry'))
+    return graphs
+
+
 if __name__ == "__main__":
-    # test code
-    ## test get_pie_foundry_dittr
-    # return_figures1()
+    # # test code
+    graphs = get_graph_by_type()
+    for fig in graphs:
+        fig.show()
 
-    fig1 = get_pie_foundry_dittr()
-    print(type(fig1))
-    fig1.show()
 
-    ## test get_bar_foundry_dittr
-    fig2 = get_bar_foundry_dittr()
-    print(type(fig2))
-    fig2.show()
+    graphs = get_graph_by_foundry()
+    for fig in graphs:
+        fig.show()
 
-    ## test get_corr
-    fig3 = get_corr()
-    print(type(fig3))
-    fig3.show()
 
-    ## test get_scatter
-    fig4 = get_scatter(cat_data, x="Die Size (mm^2)", y="TDP (W)", trendline="ols")
-    print(type(fig4))
-    fig4.show()
-
-    fig5 = get_scatter(cat_data, x="Die Size (mm^2)", y="Transistors (million)", trendline="ols")
-    print(type(fig5))
-    fig5.show()
-
-    fig6 = get_scatter(cat_data, x="Release Date", y="Freq (MHz)", color="Type")
-    print(type(fig6))
-    fig6.show()
+    graphs = get_graph_by_vendor()
+    for fig in graphs:
+        fig.show()
--- monthly_proj/eda/views.py
+++ monthly_proj/eda/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render
 import plotly
 # from io import StringIO
 
-from .graphs.get_graph import return_figures1
+from .graphs.get_graph import get_graph_by_type, get_graph_by_vendor, get_graph_by_foundry
 
 def index_view(request):
     context={}
@@ -17,9 +17,13 @@ def index_view(request):
     return render(request, 'index.html', context)
 
 
+
+# 프로세서 타입에 따른 그래프
 def page1_view(request):
     context={}
-    fig1, fig2, fig3, fig4, fig5, fig6 = return_figures1()
+    li = get_graph_by_type()
+    fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8 = li[0], li[1], li[2], li[3], li[4], li[5], li[6], li[7]
+
     graph_div = plotly.offline.plot(fig1, auto_open = False, output_type="div")
     context['grp1']=graph_div
     graph_div = plotly.offline.plot(fig2, auto_open = False, output_type="div")
@@ -32,16 +36,51 @@ def page1_view(request):
     context['grp5']=graph_div
     graph_div = plotly.offline.plot(fig6, auto_open = False, output_type="div")
     context['grp6']=graph_div
+    graph_div = plotly.offline.plot(fig7, auto_open = False, output_type="div")
+    context['grp7']=graph_div
+    graph_div = plotly.offline.plot(fig8, auto_open = False, output_type="div")
+    context['grp8']=graph_div
     
 
     return render(request, 'page1.html', context)
 
 def page2_view(request):
     context={}
+    li = get_graph_by_vendor()
+    fig1, fig2, fig3, fig4, fig5, fig6= li[0], li[1], li[2], li[3], li[4], li[5]
+
+    graph_div = plotly.offline.plot(fig1, auto_open = False, output_type="div")
+    context['grp1']=graph_div
+    graph_div = plotly.offline.plot(fig2, auto_open = False, output_type="div")
+    context['grp2']=graph_div
+    graph_div = plotly.offline.plot(fig3, auto_open = False, output_type="div")
+    context['grp3']=graph_div
+    graph_div = plotly.offline.plot(fig4, auto_open = False, output_type="div")
+    context['grp4']=graph_div
+    graph_div = plotly.offline.plot(fig5, auto_open = False, output_type="div")
+    context['grp5']=graph_div
+    graph_div = plotly.offline.plot(fig6, auto_open = False, output_type="div")
+    context['grp6']=graph_div
+
 
     return render(request, 'page2.html', context)
 
 def page3_view(request):
     context={}
-
+    li = get_graph_by_foundry()
+    fig1, fig2, fig3, fig4, fig5, fig6, fig7= li[0], li[1], li[2], li[3], li[4], li[5], li[6]
+    graph_div = plotly.offline.plot(fig1, auto_open = False, output_type="div")
+    context['grp1']=graph_div
+    graph_div = plotly.offline.plot(fig2, auto_open = False, output_type="div")
+    context['grp2']=graph_div
+    graph_div = plotly.offline.plot(fig3, auto_open = False, output_type="div")
+    context['grp3']=graph_div
+    graph_div = plotly.offline.plot(fig4, auto_open = False, output_type="div")
+    context['grp4']=graph_div
+    graph_div = plotly.offline.plot(fig5, auto_open = False, output_type="div")
+    context['grp5']=graph_div
+    graph_div = plotly.offline.plot(fig6, auto_open = False, output_type="div")
+    context['grp6']=graph_div
+    graph_div = plotly.offline.plot(fig7, auto_open = False, output_type="div")
+    context['grp7']=graph_div
     return render(request, 'page3.html', context)
